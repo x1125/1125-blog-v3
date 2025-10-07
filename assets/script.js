d480c98a4670b9f9ab099a3e737b091fdd1abd58
1125 <git@1125.io>
2025-10-07 18:37:39 +0200

    Finalizing functionality

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,42 @@
 const markdownRenderer = window.markdownit().use(window.markdownitFootnote);
 
+const pathDelimiter = '/';
+const sectionDelimiter = ':';
 const footnoteDelimiter = '^';
 
-function filterFootnoteParameter(requestParameter) {
-    return requestParameter.split(footnoteDelimiter)[0];
-}
+const Request = {
+    path: [],
+    section: null,
+    footnote: null,
 
-function getRequestParameterWithoutFootnote() {
-    return filterFootnoteParameter(String(window.location.hash).split('#')[1]);
-}
+    parse: function(requestParameter) {
+        if (requestParameter.indexOf('#') === 0) {
+            requestParameter = requestParameter.substr(1);
+        }
+        
+        let tmp = requestParameter.split(footnoteDelimiter);
+        this.footnote = tmp[1] || null;
 
-markdownRenderer.renderer.rules.footnote_ref = function(tokens, idx, options, env, slf) {
+        tmp = tmp[0].split(sectionDelimiter);
+        this.section = tmp[1] || null;
+
+        this.path = String(tmp[0]).split(pathDelimiter);
+        
+        return this;
+    },
+    
+    buildPath: function() {
+        return this.path.join(pathDelimiter);
+    },
+    buildSection: function() {
+        return this.buildPath() + sectionDelimiter + this.section;
+    },
+    buildFootnote: function() {
+        return this.buildPath() + footnoteDelimiter + this.footnote;
+    }
+};
+
+markdownRenderer.renderer.rules.footnote_ref = function (tokens, idx, options, env, slf) {
     const id = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf);
     const caption = slf.rules.footnote_caption(tokens, idx, options, env, slf);
     let refid = id;
@@ -19,10 +45,11 @@ markdownRenderer.renderer.rules.footnote_ref = function(tokens, idx, options, en
         refid += ':' + tokens[idx].meta.subId;
     }
 
-    return '<sup class="footnote-ref"><a href="#' + getRequestParameterWithoutFootnote() + footnoteDelimiter + id + '" id="' + getRequestParameterWithoutFootnote() + footnoteDelimiter + 'ref' + refid + '">' + caption + '</a></sup>';
+    return '<sup class="footnote-ref"><a href="#' + Request.buildPath() + footnoteDelimiter + id +
+        '" id="' + Request.buildPath() + footnoteDelimiter + 'ref' + refid + '">' + caption + '</a></sup>';
 };
 
-markdownRenderer.renderer.rules.footnote_anchor = function(tokens, idx, options, env, slf) {
+markdownRenderer.renderer.rules.footnote_anchor = function (tokens, idx, options, env, slf) {
     let id = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf);
 
     if (tokens[idx].meta.subId > 0) {
@@ -30,22 +57,33 @@ markdownRenderer.renderer.rules.footnote_anchor = function(tokens, idx, options,
     }
 
     /* ↩ with escape code to prevent display as Apple Emoji on iOS */
-    return ' <a href="#' + getRequestParameterWithoutFootnote() + footnoteDelimiter + 'ref' + id + '" class="footnote-backref">\u21a9\uFE0E</a>';
+    return ' <a href="#' + Request.buildPath() + footnoteDelimiter + 'ref' + id + '" class="footnote-backref">\u21a9\uFE0E</a>';
 };
 
-markdownRenderer.renderer.rules.footnote_open = function(tokens, idx, options, env, slf) {
-    var id = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf);
+markdownRenderer.renderer.rules.footnote_open = function (tokens, idx, options, env, slf) {
+    let id = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf);
 
     if (tokens[idx].meta.subId > 0) {
         id += ':' + tokens[idx].meta.subId;
     }
 
-    return '<li id="' + getRequestParameterWithoutFootnote() + footnoteDelimiter + id + '" class="footnote-item">';
+    return '<li id="' + Request.buildPath() + footnoteDelimiter + id + '" class="footnote-item">';
+};
+
+markdownRenderer.renderer.rules.heading_open = function(tokens, idx, options, env, slf) {
+    const title = tokens[idx+1].content;
+    return '<div class="sectionLink"><' + tokens[idx].tag + ' id="' + Request.buildPath() + sectionDelimiter + title + '">';
+};
+
+markdownRenderer.renderer.rules.heading_close = function(tokens, idx, options, env, slf) {
+    const title = tokens[idx-1].content;
+    return '</' + tokens[idx].tag + '><a href="#' + Request.buildPath() + sectionDelimiter + title + '" class="is-hidden"><i class="fas fa-link"></i></a></div>';
 };
 
 document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("hashchange", function (e) {
-        if (filterFootnoteParameter(e.newURL) === filterFootnoteParameter(e.oldURL)) {
+        const newRequest = new Request.parse(e.newURL.split('#')[1]);
+        if (newRequest.path.join(pathDelimiter) === Request.path.join(pathDelimiter)) {
             // no change occurred
             // required for footnotes
             return;
@@ -57,16 +95,26 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function DoRouting() {
-    const requestParameter = getRequestParameterWithoutFootnote();
+    Request.parse(window.location.hash);
 
-    if (requestParameter === '') {
+    let firstElement = '';
+    if (Request.path.length > 0) {
+        firstElement = Request.path[0];
+    }
+
+    SetActiveMenuItem(firstElement);
+
+    if (firstElement === '') {
+        window.location.hash = '#latest';
+        return;
+    }
+
+    if (firstElement === 'latest') {
         RecentlyUpdatedAction();
         return;
     }
 
-    const requestParameters = requestParameter.split(':');
-    const mdPath = 'posts/' + requestParameters[0] + '.md';
-    RenderPostAction(mdPath);
+    RenderPostAction('posts/' + firstElement + '.md');
 }
 
 function HttpGetRequest(url) {
@@ -98,12 +146,21 @@ function RenderPostAction(mdPath) {
     HttpGetRequest(mdPath).then((markdownData) => {
         SetMainContent(markdownRenderer.render(markdownData));
         ShowContentContainer('main');
+
+        // TODO: scroll to section / footnote if available
+        if (Request.section !== null) {
+            ScrollToAnchor(Request.buildSection());
+        } else if(Request.footnote !== null) {
+            ScrollToAnchor(Request.buildFootnote());
+        }
+
     }).catch((error) => {
         if (error === 404) {
             SetMessageBox('warning', 'Page not found', 'The Page could not be found, please try again');
             ShowContentContainer('message');
             return;
         }
+        console.error(error);
         SetMessageBox('danger', 'Unknown error', 'Please try reloading the page');
         ShowContentContainer('message');
     });
@@ -111,7 +168,7 @@ function RenderPostAction(mdPath) {
 
 function ShowContentContainer(type) {
     const contentContainers = document.getElementsByClassName('content-container');
-    Array.prototype.forEach.call(contentContainers, function(contentContainer) {
+    Array.prototype.forEach.call(contentContainers, function (contentContainer) {
         contentContainer.classList.add('is-hidden');
     });
     document.getElementById(type + '-container').classList.remove('is-hidden');
@@ -126,4 +183,22 @@ function SetMessageBox(type, title, body) {
     document.querySelector('#message-container').classList.add('is-' + type);
     document.querySelector('#message-container .message-header > p').innerHTML = title;
     document.querySelector('#message-container .message-body').innerHTML = body;
+}
+
+function SetActiveMenuItem(route) {
+    document.querySelectorAll('#menuList li a').forEach(function(a){
+        if (a.hash === '#' + route) {
+            a.classList.add('is-active');
+        } else {
+            a.classList.remove('is-active');
+        }
+    });
+}
+
+function ScrollToAnchor(id) {
+    const targetElement = document.getElementById(id);
+    if (!targetElement) {
+        return;
+    }
+    (document.scrollingElement || document.documentElement).scrollTop = targetElement.offsetTop;
 }
